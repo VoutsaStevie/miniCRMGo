334b8ba3eb29edc45dc411755ef836e2ddfddab9
norab0 <boudarbalanora@gmail.com>
2025-11-03 14:55:25 +0100

    Menu pricipal CRM

--- main.go
+++ main.go
@@ -5,5 +5,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("--- Mini CRM ---")
+	fmt.Println("1. lister les contacts")
+	fmt.Println("2. ajouter un contact")
+	fmt.Println("4. supprimer un contact")
+	fmt.Println("5. quitter")
+
+
 }
 
