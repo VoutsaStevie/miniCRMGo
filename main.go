f2afd2976e76bb3fd3a48e35a06ee46afc154c13
norab0 <boudarbalanora@gmail.com>
2025-11-03 15:03:17 +0100

    update choix menu crm

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("--- Mini CRM ---")
-	fmt.Println("1. lister les contacts")
-	fmt.Println("2. ajouter un contact")
+	fmt.Println("1. ajouter un contac")
+	fmt.Println("2. lister les contacts")
 	fmt.Println("4. supprimer un contact")
 	fmt.Println("5. quitter")
 var choix int
